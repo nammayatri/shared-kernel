ed5727eb3c8116244f00271a4e33fcc868678efc
Roman Bodavskiy <roman.bodavsky@juspay.in>
2022-07-02 17:36:29 +0300

    [BKN-1451] fixed ItemCode instances

--- lib/beckn-core/src/Beckn/Types/Core/Taxi/Common/ItemCode.hs
+++ lib/beckn-core/src/Beckn/Types/Core/Taxi/Common/ItemCode.hs
@@ -28,6 +28,8 @@ instance Show ItemCode where
     show ONE_WAY_TRIP <> "_" <> show vehVar
   show (ItemCode RENTAL_TRIP vehVar (Just dist) (Just dur)) =
     show RENTAL_TRIP <> "_" <> show vehVar <> "_" <> show dist <> "_" <> show dur
+  show (ItemCode AUTO_TRIP vehVar Nothing Nothing) =
+    show AUTO_TRIP <> "_" <> show vehVar
   show _ = error "ItemCode content doesn't correspond its FareProductType/"
 
 instance Read ItemCode where
@@ -47,6 +49,10 @@ instance Read ItemCode where
                    r5 <- stripPrefix "_" r4,
                    (v3, r6) <- readsPrec (app_prec + 1) r5
                ]
+            ++ [ (ItemCode AUTO_TRIP v1 Nothing Nothing, r2)
+                  | r1 <- stripPrefix "AUTO_TRIP_" r,
+                    (v1, r2) <- readsPrec (app_prec + 1) r1
+                ]
       )
       r'
     where
