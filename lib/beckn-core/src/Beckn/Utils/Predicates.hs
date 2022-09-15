14699606ed147c3e7ef0af02d741c5522c117821
Aman Kumar Dwibedi <aman.dwibedi@juspay.in>
2022-09-15 20:48:17 +0530

    restricted country code to accept only +91

--- lib/beckn-core/src/Beckn/Utils/Predicates.hs
+++ lib/beckn-core/src/Beckn/Utils/Predicates.hs
@@ -3,10 +3,10 @@
 module Beckn.Utils.Predicates where
 
 import Beckn.Types.Predicate
-import EulerHS.Prelude
+import Beckn.Prelude
 
 digit, latinUC, latinLC, latin, alphanum, latinOrSpace :: Regex
-digit = charRange '0' '9'
+digit = charRange '0' '9' 
 latinUC = charRange 'A' 'Z'
 latinLC = charRange 'a' 'z'
 latin = latinUC \/ latinLC
@@ -16,5 +16,8 @@ latinOrSpace = latin \/ " "
 mobileNumber = ExactLength 10 `And` star digit
 
 mobileCountryCode = LengthInRange 2 4 `And` ("+" <> star digit)
+
+mobileIndianCode :: Regex
+mobileIndianCode = "+91"
 
 name = star latinOrSpace
