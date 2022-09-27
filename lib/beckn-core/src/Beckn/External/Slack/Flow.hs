79f80fc0ec4fcc08b305804f2e5ac92b6d4143b4
Hemant Mangla <hemant.mangla@juspay.in>
2022-09-27 23:27:31 +0530

    BKN-2013: add slack post message

---
+++ lib/beckn-core/src/Beckn/External/Slack/Flow.hs
@@ -0,0 +1,58 @@
+module Beckn.External.Slack.Flow where
+
+import Beckn.External.Slack.Types
+import Beckn.Tools.Metrics.CoreMetrics (CoreMetrics)
+import Beckn.Types.Common
+import Beckn.Utils.Common hiding (Error)
+import Data.Maybe
+import qualified Data.Text as T
+import EulerHS.Prelude
+import qualified EulerHS.Types as ET
+import Servant hiding (throwError)
+import Servant.Client
+
+-- | Slack API interface
+type SlackConnectAPI =
+  Header "token" Text
+    :> ReqBody '[JSON] SlackRequest
+    :> Post '[JSON] SlackResponse
+
+slackConnectAPI :: Proxy SlackConnectAPI
+slackConnectAPI = Proxy
+
+defaultBaseUrl :: BaseUrl
+defaultBaseUrl =
+  BaseUrl
+    { baseUrlScheme = Https,
+      baseUrlHost = "slack.com",
+      baseUrlPort = 443,
+      baseUrlPath = T.unpack "/api/chat.postMessage"
+    }
+
+postMessage ::
+  ( CoreMetrics m,
+    HasFlowEnv m r '["slackCfg" ::: SlackConfig]
+  ) =>
+  T.Text ->
+  m SlackResponse
+postMessage message = do
+  withLogTag "Slack" $ do
+    SlackConfig {..} <- asks (.slackCfg)
+    let slackRequest = 
+          SlackRequest
+            { channel = channelName
+            , blocks = Just [Block { _type = "section", _text = Block { _type = "text", _text = message } }]
+            }
+    callSlackAPI
+      defaultBaseUrl
+      (callSlack slackToken slackRequest)
+      "PostMessage"
+  where
+    callSlack token slackRequest = ET.client slackConnectAPI (Just token) slackRequest
+
+callSlackAPI :: CallAPI env a
+callSlackAPI =
+  callApiUnwrappingApiError
+    (identity @Error)
+    Nothing
+    Nothing
