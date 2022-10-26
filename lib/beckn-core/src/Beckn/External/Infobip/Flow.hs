b94ca26e1d1d5ab4668a4887d6f6f28ec9c07ebb
Pranav Mishra <pranav.mishra@juspay.in>
2022-10-26 09:55:29 -0700

    BKN-2135/infobip

---
+++ lib/beckn-core/src/Beckn/External/Infobip/Flow.hs
@@ -0,0 +1,46 @@
+module Beckn.External.Infobip.Flow where
+
+import qualified Beckn.External.Infobip.API as API
+import Beckn.External.Infobip.Types
+import Beckn.Tools.Metrics.CoreMetrics (CoreMetrics)
+import Beckn.Types.Common
+import Beckn.Types.Error
+import Beckn.Utils.Common
+import qualified Data.Text.Encoding as T
+import qualified Data.Text as T
+import EulerHS.Prelude
+import qualified Data.ByteString.Base64 as Base64
+
+sendSms ::
+  ( CoreMetrics m,
+    MonadFlow m
+  ) =>
+  InfobipConfig ->
+  Text ->
+  Text ->
+  m SMSRes
+sendSms smsCfg smsTemplate phoneNumber = do
+  let  url = smsCfg.url
+  let req = SMSReq { messages = [
+        MessageReq
+          { destinations = [ SMSDestination phoneNumber ],
+            from = smsCfg.sender,
+            text = smsTemplate
+          }
+      ]
+    }
+  let userpass = fromString $ (T.unpack smsCfg.username) <> ":" <> (T.unpack smsCfg.password)
+  let auth = "Basic " <> T.decodeUtf8 (Base64.encode userpass)
+  submitSms url auth req
+
+submitSms ::
+  ( CoreMetrics m,
+    MonadFlow m
+  ) =>
+  BaseUrl ->
+  Text ->
+  SMSReq ->
+  m SMSRes
+submitSms url auth req = do
+  callAPI url (API.sendSms auth req) "sendSms"
+    >>= fromEitherM (ExternalAPICallError (Just "UNABLE_TO_SEND_SMS") url)
---
+++ lib/beckn-core/src/Beckn/External/Infobip/Types.hs
@@ -0,0 +1,56 @@
+module Beckn.External.Infobip.Types where
+
+import EulerHS.Prelude hiding (encodeUtf8, fromStrict, toStrict, id)
+import Servant.Client (BaseUrl)
+import Beckn.Utils.Dhall (FromDhall)
+
+newtype SMSReq = SMSReq { messages :: [MessageReq] }
+  deriving (Generic, FromJSON, ToJSON, Show, Eq, FromDhall)
+
+data SMSRes = SMSRes 
+  { bulkId :: Maybe Text,
+    messages :: Maybe [MessageRes],
+    requestError :: Maybe SMSErr
+  }
+  deriving (Generic, FromJSON, ToJSON, Show, Eq, FromDhall)
+
+data SMSErr = SMSErr 
+  { messageId :: Text,
+    text :: Text,
+    validationErrors :: Text
+  }
+  deriving (Generic, FromJSON, ToJSON, Show, Eq, FromDhall)
+
+data MessageReq = MessageReq 
+  { destinations :: [SMSDestination],
+    from :: Text,
+    text :: Text  
+  }
+  deriving (Generic, FromJSON, ToJSON, Show, Eq, FromDhall)
+
+newtype SMSDestination = SMSDestination { to :: Text}
+  deriving (Generic, FromJSON, ToJSON, Show, Eq, FromDhall)
+
+data MessageRes = MessageRes 
+  { messageId :: Text,
+    status :: SMSStatus,
+    to :: Text
+  }
+  deriving (Generic, FromJSON, ToJSON, Show, Eq, FromDhall)
+
+data SMSStatus = SMSStatus 
+  { description :: Text,
+    groupId :: Int,
+    groupName :: Text,
+    id :: Int,
+    name :: Text
+  }
+  deriving (Generic, FromJSON, ToJSON, Show, Eq, FromDhall)
+
+data InfobipConfig = InfobipConfig 
+  { username :: Text,
+    password :: Text,
+    url :: BaseUrl,
+    sender :: Text
+  }
+  deriving (Generic, FromJSON, ToJSON, Show, Eq, FromDhall)
