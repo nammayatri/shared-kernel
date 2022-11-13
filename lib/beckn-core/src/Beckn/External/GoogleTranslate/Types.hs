dd902f76e50cfc5715abb254726b23fca2aa812b
Utkarsh <utkarsh.pandey1@juspay.in>
2022-11-13 15:30:19 +0530

    added_has_google_translate_constraint

--- lib/beckn-core/src/Beckn/External/GoogleTranslate/Types.hs
+++ lib/beckn-core/src/Beckn/External/GoogleTranslate/Types.hs
@@ -5,11 +5,12 @@ module Beckn.External.GoogleTranslate.Types where
 import Beckn.Prelude
 import Beckn.Utils.JSON
 
+type HasGoogleTranslate m r = (MonadReader r m, HasField "googleTranslateUrl" r BaseUrl, HasField "googleTranslateKey" r Text)
 data TranslateResp = TranslateResp
   { _data :: Translations,
     _error :: Maybe TranslateError
   }
-  deriving (Generic, ToSchema)
+  deriving (Generic, Show, ToSchema)
 
 instance FromJSON TranslateResp where
   parseJSON = genericParseJSON stripPrefixUnderscoreIfAny
@@ -20,14 +21,14 @@ instance ToJSON TranslateResp where
 newtype TranslateError = TranslateError
   { code :: Text
   }
-  deriving (Generic, FromJSON, ToJSON, ToSchema)
+  deriving (Generic, FromJSON, ToJSON, ToSchema, Show)
 
 newtype Translations = Translation
   { translations :: [TranslatedText]
   }
-  deriving (Generic, ToJSON, FromJSON, ToSchema)
+  deriving (Generic, ToJSON, FromJSON, ToSchema, Show)
 
 newtype TranslatedText = TranslatedText
   { translatedText :: Text
   }
-  deriving (Generic, ToJSON, FromJSON, ToSchema)
+  deriving (Generic, ToJSON, FromJSON, ToSchema, Show)
