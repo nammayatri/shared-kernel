85d1befa12b097a719f3c68e87666fbb9f686bcf
Sridhar Ratnakumar <srid@srid.ca>
2023-03-04 12:17:30 -0500

    nix: expose flake modules

--- flake.nix
+++ flake.nix
@@ -11,20 +11,37 @@
     euler-hs.url = "github:srid/euler-hs/ghc810--nixify";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, ... }: {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         inputs.common.flakeModules.default
       ];
+
       perSystem = { self', pkgs, lib, config, ... }: {
         cachix-push.packages = [ "default" ];
         packages.default = self'.packages.mobility-core;
         haskellProjects.default = {
           imports = [
-            inputs.euler-hs.haskellFlakeProjectModules.output
+            self.haskellFlakeProjectModules.input
           ];
           basePackages = config.haskellProjects.ghc810.outputs.finalPackages;
           packages.mobility-core.root = ./lib/mobility-core;
+        };
+      };
+
+      flake.haskellFlakeProjectModules = rec {
+        output = { pkgs, lib, ... }: {
+          imports = [ input local ];
+        };
+        local = { pkgs, lib, ... }: withSystem pkgs.system ({ config, ... }: {
+          source-overrides =
+            lib.mapAttrs (name: ks: ks.root)
+              config.haskellProjects.default.packages;
+        });
+        input = { pkgs, lib, ... }: {
+          imports = [
+            inputs.euler-hs.haskellFlakeProjectModules.output
+          ];
           source-overrides = {
             passetto-client = inputs.passetto-hs + /client;
             passetto-core = inputs.passetto-hs + /core;
@@ -34,6 +51,6 @@
           };
         };
       };
-    };
+    });
 
 }
