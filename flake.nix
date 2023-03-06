2830f204eee5b1998c1fe6eeb48a1d3d73562845
Sridhar Ratnakumar <srid@srid.ca>
2023-03-06 14:08:20 -0500

    Try autogenerated modules

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    haskell-flake.url = "path:/Users/srid/code/haskell-flake";
     common.url = "github:nammayatri/common";
+    common.inputs.haskell-flake.follows = "haskell-flake";
     flake-parts.follows = "common/flake-parts";
 
     passetto-hs.url = "github:juspay/passetto/bb92cf1dd9699662d2a7bb96cd6a6aed6f20e8ff";
@@ -9,6 +11,7 @@
 
     # euler-hs and its transitive dependencies
     euler-hs.url = "github:srid/euler-hs/ghc810--nixify";
+    euler-hs.inputs.haskell-flake.follows = "haskell-flake";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, ... }: {
@@ -39,30 +42,6 @@
         };
       };
 
-      # Typically the consumer of this flake will want to use one of the
-      # following modules:
-      #
-      # - output: provides both local package and dependency overrides.
-      # - local: provides only local package overrides (ignores dependency
-      #   overrides in this flake)
-      flake.haskellFlakeProjectModules = rec {
-        # The 'output' module provides both local package and dependency
-        # overrides.
-        output = {
-          imports = [ input local ];
-        };
-        # The 'local' module provides only local package overrides.
-        local = { pkgs, lib, ... }: withSystem pkgs.system ({ config, ... }: {
-          source-overrides =
-            lib.mapAttrs (_: v: v.root)
-              config.haskellProjects.default.packages;
-        });
-        # The 'input' module contains only dependency overrides.
-        input = { pkgs, ... }: withSystem pkgs.system ({ config, ... }: {
-          inherit (config.haskellProjects.default)
-            source-overrides overrides;
-        });
-      };
     });
 
 }
