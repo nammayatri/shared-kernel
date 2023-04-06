a056ff3ee3ce5f4b50bbc9cb2af597599bc93700
soumyajit <soumyajit.behera@juspay.in>
2023-04-06 15:23:42 +0530

    backend/fix/#71 : added first digit validation

--- lib/mobility-core/src/Kernel/Utils/Predicates.hs
+++ lib/mobility-core/src/Kernel/Utils/Predicates.hs
@@ -20,13 +20,14 @@ import Kernel.Types.Predicate
 
 digit, latinUC, latinLC, latin, alphanum, latinOrSpace :: Regex
 digit = charRange '0' '9'
+firstDigit = charRange '6' '9'
 latinUC = charRange 'A' 'Z'
 latinLC = charRange 'a' 'z'
 latin = latinUC \/ latinLC
 alphanum = latin \/ digit
 latinOrSpace = latin \/ " "
 
-mobileNumber = ExactLength 10 `And` star digit
+mobileNumber = ExactLength 10 `And` (firstDigit <> star digit)
 
 mobileCountryCode = LengthInRange 2 4 `And` ("+" <> star digit)
 
