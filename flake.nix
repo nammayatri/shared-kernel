ff745167f162651d47667223516815bd95e4e8ac
Sridhar Ratnakumar <srid@srid.ca>
2023-04-10 17:34:23 -0400

    Improve Nix support, and use it for dev

--- flake.nix
+++ flake.nix
@@ -1,27 +1,25 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     common.url = "github:nammayatri/common";
+    nixpkgs.follows = "common/nixpkgs";
     flake-parts.follows = "common/flake-parts";
+    systems.url = "github:nix-systems/default";
 
     passetto-hs.url = "github:juspay/passetto/bb92cf1dd9699662d2a7bb96cd6a6aed6f20e8ff";
     passetto-hs.flake = false;
     clickhouse-haskell.url = "github:piyushKumar-1/clickhouse-haskell";
     clickhouse-haskell.flake = false;
 
-    # euler-hs and its transitive dependencies
     euler-hs.url = "github:srid/euler-hs/ghc810--nixify";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, ... }: {
-      systems = nixpkgs.lib.systems.flakeExposed;
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
       imports = [
         inputs.common.flakeModules.default
       ];
 
       perSystem = { self', pkgs, lib, config, ... }: {
-        cachix-push.packages = [ "default" ];
-        packages.default = self'.packages.mobility-core;
         haskellProjects.default = {
           imports = [
             inputs.euler-hs.haskellFlakeProjectModules.output
@@ -29,17 +27,25 @@
           source-overrides = {
             passetto-client = inputs.passetto-hs + /client;
             passetto-core = inputs.passetto-hs + /core;
-            clickhouse-haskell = inputs.clickhouse-haskell;
+            inherit (inputs) clickhouse-haskell;
           };
           overrides = self: super:
             with pkgs.haskell.lib.compose;
-            lib.mapAttrs (k: v: lib.pipe super.${k} v) {
+            lib.mapAttrs (k: lib.pipe super.${k}) {
               # Tests and documentation generation fail for some reason.
               euler-hs = [ dontCheck dontHaddock ];
               clickhouse-haskell = [ doJailbreak ];
             };
-          packages.mobility-core.root = ./lib/mobility-core;
+          autoWire = [ "packages" "checks" ];
+        };
+        packages.default = self'.packages.mobility-core;
+        devShells.default = pkgs.mkShell {
+          # cf. https://haskell.flake.page/devshell#composing-devshells
+          inputsFrom = [
+            config.haskellProjects.default.outputs.devShell
+            config.pre-commit.devShell
+          ];
         };
       };
-    });
+    };
 }
