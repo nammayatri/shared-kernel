f7281af4db227b534a89ab7e1a30e729de98b07f
Sridhar Ratnakumar <srid@srid.ca>
2023-05-24 20:20:34 -0400

    nix: haskell-flake: modular overrides
    Update haskell-flake to use [srid/haskell-flake#162](https://github.com/srid/haskell-flake/pull/162) which gets us
    
    - a nicer API for overriding Haskell packages.
    - reduced duplicate builds (due to use `build-haskell-package.nix` for dependencies and local packages alike.
    
    Also,
    
    - add default overrides for all local packages. This should also reduce build times across repos by reusing the same package from dependency builds.

--- flake.nix
+++ flake.nix
@@ -7,11 +7,13 @@
     clickhouse-haskell.url = "github:nammayatri/clickhouse-haskell";
     clickhouse-haskell.inputs.common.follows = "common";
     prometheus-haskell.url = "github:juspay/prometheus-haskell/more-proc-metrics";
+    prometheus-haskell.inputs.haskell-flake.follows = "common/haskell-flake";
 
-    euler-hs.url = "github:juspay/euler-hs";
+    euler-hs.url = "github:nammayatri/euler-hs/haskell-flake-0.4"; # https://github.com/juspay/euler-hs/pull/9
   };
   outputs = inputs:
     inputs.common.lib.mkFlake { inherit inputs; } {
+      debug = true;
       perSystem = { self', pkgs, lib, config, ... }: {
         haskellProjects.default = {
           imports = [
@@ -19,18 +21,22 @@
             inputs.clickhouse-haskell.haskellFlakeProjectModules.output
             inputs.prometheus-haskell.haskellFlakeProjectModules.output
           ];
-          source-overrides = {
-            passetto-client = inputs.passetto-hs + /client;
-            passetto-core = inputs.passetto-hs + /core;
+          packages = {
+            passetto-client.source = inputs.passetto-hs + /client;
+            passetto-core.source = inputs.passetto-hs + /core;
           };
-          overrides = self: super:
-            with pkgs.haskell.lib.compose;
-            lib.mapAttrs (k: lib.pipe super.${k}) {
-              # Tests and documentation generation fail for some reason.
-              euler-hs = [ dontCheck dontHaddock ];
-              wai-middleware-prometheus = [ dontCheck dontHaddock ];
-              clickhouse-haskell = [ doJailbreak ];
+          settings = {
+            # Tests and documentation generation fail for some reason.
+            euler-hs = {
+              check = false;
+              haddock = false;
             };
+            wai-middleware-prometheus = {
+              check = false;
+              haddock = false;
+            };
+            clickhouse-haskell.jailbreak = true;
+          };
           autoWire = [ "packages" "checks" ];
         };
         packages.default = self'.packages.mobility-core;
