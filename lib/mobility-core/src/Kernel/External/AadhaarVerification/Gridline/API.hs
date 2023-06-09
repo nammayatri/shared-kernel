7a3da1d4f37fb68f374881cec374e62e73129a6b
ashish saini <ashish.saini@ashish.saini-MacBookPro>
2023-06-09 22:22:40 +0530

    backend/feat : aaded aadhaar verification for Kolkata

---
+++ lib/mobility-core/src/Kernel/External/AadhaarVerification/Gridline/API.hs
@@ -0,0 +1,68 @@
+{-
+  Copyright 2022-23, Juspay India Pvt Ltd
+
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is
+
+  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+
+  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero
+
+  General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+
+module Kernel.External.AadhaarVerification.Gridline.API
+  ( GenerateAadhaarOtpAPI,
+    generateAadhaarOtpAPI,
+    generateAadhaarOtp,
+    VerifyAadhaarOtpAPI,
+    verifyAadhaarOtpAPI,
+    verifyAadhaarOtp,
+  )
+where
+
+import qualified EulerHS.Types as T
+import qualified Kernel.External.AadhaarVerification.Gridline.Types as Gridline
+import Kernel.Prelude
+import Servant (Header, JSON, Post, ReqBody, (:>))
+
+type GenerateAadhaarOtpAPI =
+  "aadhaar-api"
+    :> "boson"
+    :> "generate-otp"
+    :> Header "X-API-Key" Text
+    :> Header "X-Auth-Type" Text
+    :> ReqBody '[JSON] Gridline.GridlineAadhaarOtpReq
+    :> Post '[JSON] Gridline.GridlineVerifyAadhaarResp
+
+generateAadhaarOtpAPI :: Proxy GenerateAadhaarOtpAPI
+generateAadhaarOtpAPI = Proxy
+
+generateAadhaarOtp ::
+  Maybe Text ->
+  Maybe Text ->
+  Gridline.GridlineAadhaarOtpReq ->
+  T.EulerClient Gridline.GridlineVerifyAadhaarResp
+generateAadhaarOtp = T.client generateAadhaarOtpAPI
+
+type VerifyAadhaarOtpAPI =
+  "aadhaar-api"
+    :> "boson"
+    :> "submit-otp"
+    :> Header "X-API-Key" Text
+    :> Header "X-Auth-Type" Text
+    :> Header "X-Transaction-ID" Text
+    :> ReqBody '[JSON] Gridline.GridlineAadhaarOtpVerifyReq
+    :> Post '[JSON] Gridline.GridlineSubmitResponse
+
+verifyAadhaarOtpAPI :: Proxy VerifyAadhaarOtpAPI
+verifyAadhaarOtpAPI = Proxy
+
+verifyAadhaarOtp ::
+  Maybe Text ->
+  Maybe Text ->
+  Maybe Text ->
+  Gridline.GridlineAadhaarOtpVerifyReq ->
+  T.EulerClient Gridline.GridlineSubmitResponse
+verifyAadhaarOtp = T.client verifyAadhaarOtpAPI
---
+++ lib/mobility-core/src/Kernel/External/AadhaarVerification/Gridline/Types.hs
@@ -0,0 +1,115 @@
+{-
+ Copyright 2022-23, Juspay India Pvt Ltd
+
+ This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+ as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program
+
+ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+
+ or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of
+
+ the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+{-# LANGUAGE DerivingStrategies #-}
+
+module Kernel.External.AadhaarVerification.Gridline.Types where
+
+import Data.Aeson
+import EulerHS.Prelude hiding (error, state, (.=))
+
+data GridlineAadhaarOtpReq = GridlineAadhaarOtpReq
+  { aadhaar_number :: Text,
+    consent :: Text
+  }
+  deriving (Generic, FromJSON, ToJSON, Show)
+
+data GridlineVerifyAadhaarData = GridlineVerifyAadhaarData
+  { code :: Text,
+    message :: Text,
+    transaction_id :: Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data GridlineVerifyAadhaarError = GridlineVerifyAadhaarError
+  { code :: Text,
+    message :: Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data GridlineVerifyAadhaarResp = GridlineVerifyAadhaarResp
+  { request_id :: Text,
+    status :: Int,
+    _data :: Maybe GridlineVerifyAadhaarData,
+    _error :: Maybe GridlineVerifyAadhaarError,
+    path :: Text
+  }
+  deriving (Show, Generic, ToJSON)
+
+instance FromJSON GridlineVerifyAadhaarResp where
+  parseJSON = withObject "GridlineVerifyAadhaarResp" $ \v -> do
+    request_id <- v .: "request_id"
+    path <- v .: "path"
+    status <- v .: "status"
+    _data <- v .:? "data"
+    _error <- v .:? "error"
+    return (GridlineVerifyAadhaarResp request_id status _data _error path)
+
+data GridlineAadhaarOtpVerifyReq = GridlineAadhaarOtpVerifyReq
+  { otp :: Int,
+    include_xml :: Bool,
+    share_code :: Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data GridlineSubmitResponse = GridlineSubmitResponse
+  { _data :: Maybe GridlineSubmitAadhaarData,
+    _error :: Maybe GridlineSubmitAadhaarError,
+    request_id :: Text
+  }
+  deriving (Show, Generic, ToJSON)
+
+data GridlineSubmitAadhaarData = GridlineSubmitAadhaarData
+  { aadhaar_data :: AadhaarData,
+    code :: Text,
+    transaction_id :: Text,
+    message :: Text,
+    share_code :: Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data AadhaarData = AadhaarData
+  { document_type :: Text,
+    name :: Text,
+    date_of_birth :: Text,
+    gender :: Text,
+    care_of :: Text,
+    house :: Text,
+    street :: Text,
+    district :: Text,
+    sub_district :: Text,
+    landmark :: Text,
+    locality :: Text,
+    post_office_name :: Text,
+    state :: Text,
+    pincode :: Text,
+    country :: Text,
+    vtc_name :: Text,
+    mobile :: Text,
+    email :: Text,
+    photo_base64 :: Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+data GridlineSubmitAadhaarError = GridlineSubmitAadhaarError
+  { message :: Text,
+    cause :: Text
+  }
+  deriving (Show, Generic, ToJSON, FromJSON)
+
+instance FromJSON GridlineSubmitResponse where
+  parseJSON = withObject "GridlineSubmitResponse" $ \v -> do
+    _data <- v .:? "data"
+    _error <- v .:? "error"
+    request_id <- v .: "request_id"
+    return (GridlineSubmitResponse _data _error request_id)
