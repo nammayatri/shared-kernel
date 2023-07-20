35a5339b765c17cdd49972c7bbab309dd1f68b82
Utkarsh Pandey1 <utkarsh.pandey1@utkarshpandey1-H9MQF9L7KF.local>
2023-07-20 16:09:18 +0530

    backend/feat/register_mandate_api

--- lib/mobility-core/src/Kernel/External/Payment/Interface/Juspay.hs
+++ lib/mobility-core/src/Kernel/External/Payment/Interface/Juspay.hs
@@ -17,6 +17,7 @@ module Kernel.External.Payment.Interface.Juspay
     createOrder,
     orderStatus,
     orderStatusWebhook,
+    registerMandate,
   )
 where
 
@@ -44,6 +45,18 @@ createOrder config req = do
       merchantId = config.merchantId
   apiKey <- decrypt config.apiKey
   Juspay.createOrder url apiKey merchantId (mkCreateOrderReq config.returnUrl req)
+
+registerMandate ::
+  ( Metrics.CoreMetrics m,
+    EncFlow m r
+  ) =>
+  JuspayCfg ->
+  RegisterMandateReq ->
+  m RegisterMandateResp
+registerMandate config req = do
+  let url = config.url
+  apiKey <- decrypt config.apiKey
+  Juspay.registerMandate url apiKey req
 
 mkCreateOrderReq :: BaseUrl -> CreateOrderReq -> Juspay.CreateOrderReq
 mkCreateOrderReq returnUrl CreateOrderReq {..} =
