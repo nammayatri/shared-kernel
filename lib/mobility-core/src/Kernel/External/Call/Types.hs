d6e8e8ef47dd533d9fc6df83c9553c95d347b244
janhavi.soni <janhavi.soni097@gmail.com>
2023-08-25 22:36:44 +0530

    backend/feat/#2614 : added knowlarity service for call backup

--- lib/mobility-core/src/Kernel/External/Call/Types.hs
+++ lib/mobility-core/src/Kernel/External/Call/Types.hs
@@ -23,7 +23,7 @@ import EulerHS.Prelude
 import Kernel.Beam.Lib.UtilsTH (mkBeamInstancesForEnum)
 import Kernel.Storage.Esqueleto (derivePersistField)
 
-data CallService = Exotel
+data CallService = Exotel | Knowlarity
   deriving (Show, Read, Eq, Ord, Generic, ToJSON, FromJSON, ToSchema)
 
 $(mkBeamInstancesForEnum ''CallService)
