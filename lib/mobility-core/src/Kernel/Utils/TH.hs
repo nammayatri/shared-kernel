abe86f4801824495e11920ff4a38f8fc43e5f704
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2023-08-29 18:35:29 +0300

    backend/refactor: th for http instances

--- lib/mobility-core/src/Kernel/Utils/TH.hs
+++ lib/mobility-core/src/Kernel/Utils/TH.hs
@@ -19,10 +19,14 @@
 
 module Kernel.Utils.TH where
 
+import qualified Data.Bifunctor as BF
+import qualified Data.ByteString.Lazy as BSL
 import Data.OpenApi (ToSchema)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as DT
 import EulerHS.Prelude
 import qualified Language.Haskell.TH as TH
-import Servant (FromHttpApiData, ToHttpApiData)
+import Servant (FromHttpApiData (..), ToHttpApiData (..))
 
 -- | A set of instances common for all identifier newtypes.
 deriveIdentifierInstances :: TH.Name -> TH.Q [TH.Dec]
@@ -42,4 +46,30 @@ deriveIdentifierInstances name = do
     deriving newtype instance FromHttpApiData $tyQ
 
     deriving newtype instance ToSchema $tyQ
+    |]
+
+mkHttpInstancesForEnum :: TH.Name -> TH.Q [TH.Dec]
+mkHttpInstancesForEnum name = do
+  fromInstance <- mkFromHttpInstanceForEnum name
+  toInstance <- mkToHttpInstanceForEnum name
+  pure $ fromInstance <> toInstance
+
+mkFromHttpInstanceForEnum :: TH.Name -> TH.Q [TH.Dec]
+mkFromHttpInstanceForEnum name = do
+  let tyQ = pure (TH.ConT name)
+  [d|
+    instance FromHttpApiData $tyQ where
+      parseUrlPiece = parseHeader . DT.encodeUtf8
+      parseQueryParam = parseUrlPiece
+      parseHeader = BF.first T.pack . eitherDecode . BSL.fromStrict
+    |]
+
+mkToHttpInstanceForEnum :: TH.Name -> TH.Q [TH.Dec]
+mkToHttpInstanceForEnum name = do
+  let tyQ = pure (TH.ConT name)
+  [d|
+    instance ToHttpApiData $tyQ where
+      toUrlPiece = DT.decodeUtf8 . toHeader
+      toQueryParam = toUrlPiece
+      toHeader = BSL.toStrict . encode
     |]
