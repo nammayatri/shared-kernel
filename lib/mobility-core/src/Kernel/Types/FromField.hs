03b45f8b9d4ab355dc9128cb2358497b1f3e9970
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2023-09-06 10:05:23 +0300

    backend/refactor: refactor duplicated beam code

--- lib/mobility-core/src/Kernel/Types/FromField.hs
+++ lib/mobility-core/src/Kernel/Types/FromField.hs
@@ -45,3 +45,12 @@ fromFieldEnum f mbValue = case mbValue of
     case readMaybe (unpackChars value') of
       Just val -> pure val
       _ -> DPSF.returnError ConversionFailed f ("Could not 'read'" <> KP.show value')
+
+fromFieldDefault ::
+  (Typeable a, Read a, DPSF.FromField a) =>
+  DPSF.Field ->
+  Maybe ByteString ->
+  DPSF.Conversion a
+fromFieldDefault f mbValue = case mbValue of
+  Nothing -> DPSF.returnError UnexpectedNull f mempty
+  Just _ -> DPSF.fromField f mbValue
