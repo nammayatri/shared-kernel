9365793b3e58759666b371c91da63fc17e622cb9
ratnadeep.b <ratnadeep.b@juspay.in>
2023-11-14 00:20:25 +0530

    backend/refactor/added incoming middleware to shared kernel.

--- lib/mobility-core/src/Kernel/Tools/Metrics/Init.hs
+++ lib/mobility-core/src/Kernel/Tools/Metrics/Init.hs
@@ -13,19 +13,57 @@
 -}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-identities #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use head" #-}
 
 module Kernel.Tools.Metrics.Init where
 
-import Data.Text as DT
-import EulerHS.Prelude as E
-import Kernel.Tools.Metrics.CoreMetrics.Types
+import Data.CaseInsensitive (CI)
+import qualified Data.CaseInsensitive as CI
+import Data.Ratio ((%))
+import qualified Data.Text as DT
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8)
+import EulerHS.Prelude as E hiding (decodeUtf8)
+import Kernel.Prelude (lookup, (!!))
+import Kernel.Tools.Metrics.CoreMetrics.Types hiding (requestLatency)
 import Kernel.Utils.Monitoring.Prometheus.Servant
+import qualified Network.HTTP.Types as HTTP
 import Network.Wai (Application, Request (..))
+import qualified Network.Wai as Wai
 import Network.Wai.Handler.Warp as W
-import Network.Wai.Middleware.Prometheus
+import Network.Wai.Middleware.Prometheus (metricsApp)
 import Prometheus as P
+import qualified Prometheus as Prom
 import Prometheus.Metric.GHC (ghcMetrics)
 import Prometheus.Metric.Proc
+import System.Clock (Clock (..), TimeSpec, diffTimeSpec, getTime, toNanoSecs)
+
+{-# NOINLINE requestLatency #-}
+requestLatency :: Prom.Vector Prom.Label3 Prom.Histogram
+requestLatency =
+  Prom.unsafeRegister $
+    Prom.vector ("handler", "method", "status_code") $
+      Prom.histogram info Prom.defaultBuckets
+  where
+    info =
+      Prom.Info
+        "http_request_duration_seconds"
+        "The HTTP request latencies in seconds."
+
+{-# NOINLINE requestLatencyWithVersionLabel #-}
+requestLatencyWithVersionLabel :: Prom.Vector Prom.Label6 Prom.Histogram
+requestLatencyWithVersionLabel =
+  Prom.unsafeRegister $
+    Prom.vector ("handler", "method", "status_code", "version", "x_client_version", "x_bundle_version") $
+      Prom.histogram info Prom.defaultBuckets
+  where
+    info =
+      Prom.Info
+        "http_request_duration_seconds"
+        "The HTTP request latencies in seconds."
 
 serve :: Int -> IO ()
 serve port = do
@@ -45,3 +83,74 @@ addServantInfo version proxy app request respond =
   let mpath = getSanitizedUrl proxy request
       fullpath = DT.intercalate "/" (pathInfo request)
    in instrumentHandlerValueWithVersionLabel version.getDeploymentVersion (\_ -> "/" <> fromMaybe fullpath mpath) app request respond
+
+instrumentHandlerValueWithVersionLabel ::
+  -- | version label
+  Text ->
+  -- | The function used to derive the "handler" value in Prometheus
+  (Wai.Request -> Text) ->
+  -- | The app to instrument
+  Wai.Application ->
+  -- | The instrumented app
+  Wai.Application
+instrumentHandlerValueWithVersionLabel versionLabel = instrumentHandlerValueWithFilter (Just versionLabel) Just
+
+-- | A more flexible variant of 'instrumentHandlerValue'.  The filter can change some
+-- responses, or drop others entirely.
+instrumentHandlerValueWithFilter ::
+  -- | version label
+  Maybe Text ->
+  -- | Response filter
+  (Wai.Response -> Maybe Wai.Response) ->
+  -- | The function used to derive the "handler" value in Prometheus
+  (Wai.Request -> Text) ->
+  -- | The app to instrument
+  Wai.Application ->
+  -- | The instrumented app
+  Wai.Application
+instrumentHandlerValueWithFilter mbVersionLabel resFilter f app req respond = do
+  start <- getTime Monotonic
+  app req $ \res -> do
+    case resFilter res of
+      Nothing -> return ()
+      Just res' -> do
+        end <- getTime Monotonic
+        let method = Just $ decodeUtf8 (Wai.requestMethod req)
+        let status = Just $ T.pack (show (HTTP.statusCode (Wai.responseStatus res')))
+        let requiredHeaders = map (T.pack . show <$>) (flip lookup (Wai.requestHeaders req) . stringToCI <$> ["x-client-version", "x-bundle-version"])
+        observeSeconds mbVersionLabel (f req) method status start end requiredHeaders
+    respond res
+
+stringToCI :: String -> CI ByteString
+stringToCI = CI.mk . fromString
+
+observeSeconds ::
+  -- | version label
+  Maybe Text ->
+  -- | handler label
+  Text ->
+  -- | method
+  Maybe Text ->
+  -- | status
+  Maybe Text ->
+  -- | start time
+  TimeSpec ->
+  -- | end time
+  TimeSpec ->
+  -- | required headers
+  [Maybe Text] ->
+  IO ()
+observeSeconds mbVersionLabel handler method status start end requiredHeaders = do
+  let latency :: Double
+      latency = fromRational $ toRational (toNanoSecs (end `diffTimeSpec` start) % 1000000000)
+  case mbVersionLabel of
+    Nothing -> do
+      Prom.withLabel
+        requestLatency
+        (handler, fromMaybe "" method, fromMaybe "" status)
+        (flip Prom.observe latency)
+    Just versionLabel -> do
+      Prom.withLabel
+        requestLatencyWithVersionLabel
+        (handler, fromMaybe "" method, fromMaybe "" status, versionLabel, fromMaybe "" (requiredHeaders !! 0), fromMaybe "" (requiredHeaders !! 1))
+        (flip Prom.observe latency)
