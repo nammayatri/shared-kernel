ed575d9e8439063c1927e5cbfa254d56b4f5e39d
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-11-16 10:42:00 +0530

    refactor/nix: change "common" to a branch that disables HLS fourmolu
    Changes "common" to a branch that disables fourmolu with HLS
    and hls-forumolu-plugin.
    
    Testing if disabling fourmolu fixes the CI build error.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    common.url = "github:nammayatri/common";
+    common.url = "github:nammayatri/common/Mobility-GHC927-disable-hls-fourmolu";
     nixpkgs.follows = "common/nixpkgs";
     haskell-flake.follows = "common/haskell-flake";
 
