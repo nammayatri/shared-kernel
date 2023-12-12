865ff6580aeb394c22488ac6227cfb70ccc6d628
vaibhav dubey <vaibhav.dubey@vaibhavdubey-YN9HGXR17N.local>
2023-12-12 19:22:10 +0530

    backend/fix: added Ord to ShortId

--- lib/mobility-core/src/Kernel/Types/Id.hs
+++ lib/mobility-core/src/Kernel/Types/Id.hs
@@ -50,7 +50,7 @@ instance (MonadGuid m) => GuidLike m (Id a) where
 newtype ShortId domain = ShortId
   { getShortId :: Text
   }
-  deriving stock (Generic, Show, Eq)
+  deriving stock (Generic, Show, Eq, Ord)
   deriving newtype (ToJSON, FromJSON, ToHttpApiData, ToSchema, ToParamSchema, PrettyShow)
 
 instance FromDhall (ShortId a) where
