7019a5feea53cac148bb4dcff9f2936f50f3e175
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2024-01-10 12:42:05 +0300

    backend/refactor: added messages for clickhouse parsing errors

--- lib/mobility-core/src/Kernel/Storage/ClickhouseV2/ClickhouseValue.hs
+++ lib/mobility-core/src/Kernel/Storage/ClickhouseV2/ClickhouseValue.hs
@@ -16,47 +16,59 @@
   General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 -}
 
-module Kernel.Storage.ClickhouseV2.ClickhouseValue where
+module Kernel.Storage.ClickhouseV2.ClickhouseValue
+  ( Value (getValue),
+    ClickhouseValue (..),
+  )
+where
 
 import Data.Coerce (coerce)
 import qualified Data.Text as T
 import qualified Data.Time as Time
 import Kernel.Prelude
 import Kernel.Types.Id
+import qualified Text.Read as T
 
 newtype Value a = Value {getValue :: String}
   deriving newtype (FromJSON)
 
 class (Show a, Read a) => ClickhouseValue a where
   toClickhouseValue :: a -> Value a
-  fromClickhouseValue :: Value a -> Maybe a -- Either String a
+  fromClickhouseValue :: Value a -> Either String a
   toClickhouseValue = Value . show
-  fromClickhouseValue = readMaybe . getValue
+  fromClickhouseValue = T.readEither . getValue
 
 instance ClickhouseValue Double
 
 instance ClickhouseValue Bool where
-  fromClickhouseValue (Value "1") = Just True
-  fromClickhouseValue (Value "0") = Just False
-  fromClickhouseValue (Value "True") = Just True
-  fromClickhouseValue (Value "False") = Just False
-  fromClickhouseValue (Value "true") = Just True
-  fromClickhouseValue (Value "false") = Just False
-  fromClickhouseValue _ = Nothing
+  fromClickhouseValue (Value "1") = Right True
+  fromClickhouseValue (Value "0") = Right False
+  fromClickhouseValue (Value "True") = Right True
+  fromClickhouseValue (Value "False") = Right False
+  fromClickhouseValue (Value "true") = Right True
+  fromClickhouseValue (Value "false") = Right False
+  fromClickhouseValue _ = Left "Supported format for Bool: 0, 1, false, true, False, True"
 
 instance ClickhouseValue Time.Day
 
 instance ClickhouseValue UTCTime where
   toClickhouseValue = Value . Time.formatTime Time.defaultTimeLocale "%Y-%m-%d %H:%M:%S%Q"
-  fromClickhouseValue = Time.parseTimeM True Time.defaultTimeLocale "%Y-%m-%d %H:%M:%S%Q" . getValue
+  fromClickhouseValue = getExcept . Time.parseTimeM @Except True Time.defaultTimeLocale "%Y-%m-%d %H:%M:%S%Q" . getValue
+
+-- No instance for (MonadFail (Either String))
+newtype Except a = Except {getExcept :: Either String a}
+  deriving newtype (Monad, Applicative, Functor)
+
+instance MonadFail Except where
+  fail = Except . Left
 
 instance ClickhouseValue (Id a) where
   toClickhouseValue = Value . T.unpack . getId
-  fromClickhouseValue = Just . Id . T.pack . getValue
+  fromClickhouseValue = Right . Id . T.pack . getValue
 
 instance ClickhouseValue a => ClickhouseValue (Maybe a) where
   toClickhouseValue (Just a) = coerce @(Value a) @(Value (Maybe a)) (toClickhouseValue a)
   toClickhouseValue Nothing = Value "null"
-  fromClickhouseValue :: ClickhouseValue a => Value (Maybe a) -> Maybe (Maybe a)
-  fromClickhouseValue (Value "null") = Just Nothing
+  fromClickhouseValue :: ClickhouseValue a => Value (Maybe a) -> Either String (Maybe a)
+  fromClickhouseValue (Value "null") = Right Nothing
   fromClickhouseValue str = Just <$> fromClickhouseValue @a (coerce @(Value (Maybe a)) @(Value a) str)
