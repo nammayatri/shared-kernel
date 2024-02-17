fc698b634078e4e2a054918499ebba0052028546
khuzema786 <khomosikhuzema894@gmail.com>
2024-02-17 15:37:10 +0530

    backend: Tamil Nadu Cities Addition

--- lib/mobility-core/src/Kernel/Types/Beckn/City.hs
+++ lib/mobility-core/src/Kernel/Types/Beckn/City.hs
@@ -46,6 +46,13 @@ data City
   | Tumakuru
   | Noida
   | Gurugram
+  | Tirunelveli
+  | Thanjavur
+  | Vellore
+  | Madurai
+  | Salem
+  | Hosur
+  | Trichy
   | AnyCity
   deriving (Eq, Generic, Show, Read, ToSchema, Ord, ToParamSchema)
   deriving (PrettyShow) via Showable City
@@ -83,6 +90,20 @@ instance FromJSON City where
   parseJSON (String "Noida") = pure Noida
   parseJSON (String "std:0124") = pure Gurugram
   parseJSON (String "Gurugram") = pure Gurugram
+  parseJSON (String "std:0462") = pure Tirunelveli
+  parseJSON (String "Tirunelveli") = pure Tirunelveli
+  parseJSON (String "std:04362") = pure Thanjavur
+  parseJSON (String "Thanjavur") = pure Thanjavur
+  parseJSON (String "std:0416") = pure Vellore
+  parseJSON (String "Vellore") = pure Vellore
+  parseJSON (String "std:0452") = pure Madurai
+  parseJSON (String "Madurai") = pure Madurai
+  parseJSON (String "std:0427") = pure Salem
+  parseJSON (String "Salem") = pure Salem
+  parseJSON (String "std:04344") = pure Hosur
+  parseJSON (String "Hosur") = pure Hosur
+  parseJSON (String "std:0431") = pure Trichy
+  parseJSON (String "Trichy") = pure Trichy
   parseJSON (String _) = pure AnyCity
   parseJSON e = typeMismatch "String" e
 
@@ -102,6 +123,13 @@ instance ToJSON City where
   toJSON Tumakuru = String "std:0816"
   toJSON Noida = String "std:01189"
   toJSON Gurugram = String "std:0124"
+  toJSON Tirunelveli = String "std:0462"
+  toJSON Thanjavur = String "std:04362"
+  toJSON Vellore = String "std:0416"
+  toJSON Madurai = String "std:0452"
+  toJSON Salem = String "std:0427"
+  toJSON Hosur = String "std:04344"
+  toJSON Trichy = String "std:0431"
   toJSON AnyCity = String "*"
 
 instance FromHttpApiData City where
@@ -139,6 +167,20 @@ instance FromHttpApiData City where
       parseLowerCaseCity "noida" = Right Noida
       parseLowerCaseCity "std:0124" = Right Gurugram
       parseLowerCaseCity "gurugram" = Right Gurugram
+      parseLowerCaseCity "std:0462" = Right Tirunelveli
+      parseLowerCaseCity "tirunelveli" = Right Tirunelveli
+      parseLowerCaseCity "std:04362" = Right Thanjavur
+      parseLowerCaseCity "thanjavur" = Right Thanjavur
+      parseLowerCaseCity "std:0416" = Right Vellore
+      parseLowerCaseCity "vellore" = Right Vellore
+      parseLowerCaseCity "std:0452" = Right Madurai
+      parseLowerCaseCity "madurai" = Right Madurai
+      parseLowerCaseCity "std:0427" = Right Salem
+      parseLowerCaseCity "salem" = Right Salem
+      parseLowerCaseCity "std:04344" = Right Hosur
+      parseLowerCaseCity "hosur" = Right Hosur
+      parseLowerCaseCity "std:0431" = Right Trichy
+      parseLowerCaseCity "trichy" = Right Trichy
       parseLowerCaseCity "*" = Right AnyCity
       parseLowerCaseCity city = Left . T.pack $ ("ParseFail: Unable to parse city: " <> city)
 
@@ -158,4 +200,11 @@ instance ToHttpApiData City where
   toUrlPiece Tumakuru = "std:0816"
   toUrlPiece Noida = "std:01189"
   toUrlPiece Gurugram = "std:0124"
+  toUrlPiece Tirunelveli = "std:0462"
+  toUrlPiece Thanjavur = "std:04362"
+  toUrlPiece Vellore = "std:0416"
+  toUrlPiece Madurai = "std:0452"
+  toUrlPiece Salem = "std:0427"
+  toUrlPiece Hosur = "std:04344"
+  toUrlPiece Trichy = "std:0431"
   toUrlPiece AnyCity = "*"
