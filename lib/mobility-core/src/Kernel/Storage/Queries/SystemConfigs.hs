22e71be5f64971bd333e73a2b92a82cc8c497370
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-02-21 12:18:36 +0530

    fixed the return type

--- lib/mobility-core/src/Kernel/Storage/Queries/SystemConfigs.hs
+++ lib/mobility-core/src/Kernel/Storage/Queries/SystemConfigs.hs
@@ -2,6 +2,7 @@
 
 module Kernel.Storage.Queries.SystemConfigs where
 
+import qualified EulerHS.Language as L
 import Kernel.Beam.Functions (FromTType' (fromTType'), ToTType' (toTType'), findOneWithDb)
 import Kernel.Beam.Lib.UtilsTH
 import Kernel.Prelude
@@ -9,6 +10,7 @@ import qualified Kernel.Storage.Beam.SystemConfigs as BeamSC
 import Kernel.Storage.Esqueleto.Config
 import Kernel.Tools.Metrics.CoreMetrics.Types
 import Kernel.Types.CacheFlow (CacheFlow)
+import Kernel.Types.Error
 import Kernel.Types.SystemConfigs as Domain
 import Sequelize as Se
 
@@ -17,9 +19,10 @@ findById cfgId = do
   findOneWithDb [Se.Is BeamSC.id $ Se.Eq cfgId] <&> (<&> Domain.configValue)
     >>= maybe (incrementKvConfigFailedCounter ("kv_config_find_failed_" <> schemaName (Proxy :: Proxy BeamSC.SystemConfigsT)) >> pure Nothing) (pure . Just)
 
-findById' :: (CacheFlow m r, EsqDBFlow m r, HasSchemaName BeamSC.SystemConfigsT) => Text -> m (Maybe Domain.SystemConfigs)
+findById' :: (CacheFlow m r, EsqDBFlow m r, HasSchemaName BeamSC.SystemConfigsT) => Text -> m (Domain.SystemConfigs)
 findById' cfgId = do
-  findOneWithDb [Se.Is BeamSC.id $ Se.Eq cfgId]
+  config <- findOneWithDb [Se.Is BeamSC.id $ Se.Eq cfgId]
+  maybe (L.throwException $ InternalError "Not able to find CAC config by id") pure config
 
 instance FromTType' BeamSC.SystemConfigs Domain.SystemConfigs where
   fromTType' BeamSC.SystemConfigsT {..} = do
