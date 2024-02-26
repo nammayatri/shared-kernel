e5b49a10ec101a4397a0c9286104666a37e8dd51
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-02-26 13:57:15 +0530

    moved initializeCACThroughConfig to shared-kernel

--- lib/mobility-core/src/Kernel/Types/Cac.hs
+++ lib/mobility-core/src/Kernel/Types/Cac.hs
@@ -4,6 +4,9 @@ import Data.Aeson
 import qualified Data.Aeson.Key as DAK
 import Data.Maybe
 import qualified Data.Text as Text
+import Kernel.Prelude
+import Kernel.Utils.Common
+import qualified System.Environment as Se
 
 data CACValue a = CACValue
   { cacValue :: Value
@@ -17,3 +20,14 @@ dropPrefixFromConfig key config =
   case Text.stripPrefix key (DAK.toText config) of
     Just a -> DAK.fromText a
     Nothing -> config
+
+initializeCACThroughConfig :: (CacheFlow m r, EsqDBFlow m r) => (String -> Int -> String -> String -> IO Int) -> Text -> m ()
+initializeCACThroughConfig func config = do
+  host <- liftIO $ Se.lookupEnv "CAC_HOST"
+  interval' <- liftIO $ Se.lookupEnv "CAC_INTERVAL"
+  interval <- pure $ fromMaybe 10 (readMaybe =<< interval')
+  tenant <- liftIO (Se.lookupEnv "DRIVER_TENANT") >>= pure . fromMaybe "atlas_driver_offer_bpp_v2"
+  status <- liftIO $ func tenant interval (Text.unpack config) (fromMaybe "http://localhost:8080" host)
+  case status of
+    0 -> pure ()
+    _ -> error $ "error in creating the client for tenant" <> Text.pack tenant
