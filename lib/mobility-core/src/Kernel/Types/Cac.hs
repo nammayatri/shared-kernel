eb53e72643c011f339f4c1a3e7cadb0fc695de64
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-02-26 12:10:57 +0530

    moving drop function to shared-kernal

--- lib/mobility-core/src/Kernel/Types/Cac.hs
+++ lib/mobility-core/src/Kernel/Types/Cac.hs
@@ -1,6 +1,9 @@
 module Kernel.Types.Cac where
 
 import Data.Aeson
+import qualified Data.Aeson.Key as DAK
+import Data.Maybe
+import qualified Data.Text as Text
 
 data CACValue a = CACValue
   { cacValue :: Value
@@ -8,3 +11,9 @@ data CACValue a = CACValue
 
 class FromJSONCAC a where
   fromJSONCAC :: FromJSON a => CACValue a -> Result a
+
+dropKeyFromConfig :: Text.Text -> Key -> Key
+dropKeyFromConfig key config =
+  case Text.stripPrefix key (DAK.toText config) of
+    Just a -> DAK.fromText a
+    Nothing -> config
