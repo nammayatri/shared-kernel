0341c63acd607625ee07e7622437a06ace28bd63
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-02-26 12:25:22 +0530

    fix function name

--- lib/mobility-core/src/Kernel/Types/Cac.hs
+++ lib/mobility-core/src/Kernel/Types/Cac.hs
@@ -12,8 +12,8 @@ data CACValue a = CACValue
 class FromJSONCAC a where
   fromJSONCAC :: FromJSON a => CACValue a -> Result a
 
-dropKeyFromConfig :: Text.Text -> Key -> Key
-dropKeyFromConfig key config =
+dropPrefixFromConfig :: Text.Text -> Key -> Key
+dropPrefixFromConfig key config =
   case Text.stripPrefix key (DAK.toText config) of
     Just a -> DAK.fromText a
     Nothing -> config
