1aed2355ea31ad1b2d434bac5ef82b526ad4705a
Akhilesh Bhadauriya <akhilesh.b@juspay.in>
2024-02-28 14:53:22 +0530

    fixed the cac issue

--- lib/mobility-core/src/Kernel/Types/Cac.hs
+++ lib/mobility-core/src/Kernel/Types/Cac.hs
@@ -6,7 +6,6 @@ import Data.Maybe
 import qualified Data.Text as Text
 import Kernel.Prelude
 import Kernel.Utils.Common
-import qualified System.Environment as Se
 
 data CACValue a = CACValue
   { cacValue :: Value
@@ -21,13 +20,10 @@ dropPrefixFromConfig key config =
     Just a -> DAK.fromText a
     Nothing -> config
 
-initializeCACThroughConfig :: (CacheFlow m r, EsqDBFlow m r) => (String -> Int -> String -> String -> IO Int) -> Text -> m ()
-initializeCACThroughConfig func config = do
-  host <- liftIO $ Se.lookupEnv "CAC_HOST"
-  interval' <- liftIO $ Se.lookupEnv "CAC_INTERVAL"
-  interval <- pure $ fromMaybe 10 (readMaybe =<< interval')
-  tenant <- liftIO (Se.lookupEnv "DRIVER_TENANT") >>= pure . fromMaybe "atlas_driver_offer_bpp_v2"
-  status <- liftIO $ func tenant interval (Text.unpack config) (fromMaybe "http://localhost:8080" host)
+initializeCACThroughConfig :: (CacheFlow m r, EsqDBFlow m r) => (String -> Int -> String -> String -> IO Int) -> Text -> String -> String -> Int -> m ()
+initializeCACThroughConfig func config tenant host interval = do
+  status <- liftIO $ func tenant interval (Text.unpack config) host
+  logDebug $ "status of creating the client for tenant backup " <> Text.pack tenant <> " is " <> Text.pack (show status)
   case status of
     0 -> pure ()
     _ -> error $ "error in creating the client for tenant" <> Text.pack tenant
