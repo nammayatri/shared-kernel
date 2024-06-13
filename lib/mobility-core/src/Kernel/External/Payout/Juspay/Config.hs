5bbac59cf85f5a3bad55f1d02f0f5fc8461d86b3
Prashant Singh <prashantsingh061201@gmail.com>
2024-06-14 01:23:54 +0530

    backend/feat/d2c-referral-payout

---
+++ lib/mobility-core/src/Kernel/External/Payout/Juspay/Config.hs
@@ -0,0 +1,30 @@
+{-
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is
+
+  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+
+  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero
+
+  General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+{-# LANGUAGE DerivingStrategies #-}
+
+module Kernel.External.Payout.Juspay.Config where
+
+import Data.Aeson
+import Kernel.External.Encryption
+import Kernel.Prelude
+
+data JuspayConfig = JuspayConfig
+  { apiKey :: EncryptedField 'AsEncrypted Text,
+    returnUrl :: BaseUrl,
+    url :: BaseUrl,
+    merchantId :: Text,
+    username :: Text,
+    password :: EncryptedField 'AsEncrypted Text,
+    gatewayReferenceId :: Maybe Text
+  }
+  deriving stock (Show, Eq, Generic)
+  deriving anyclass (FromJSON, ToJSON)
