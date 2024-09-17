f39d62b9e510b29d73aa534702b8df21adaa2025
Vijay Gupta <vijayrauniyar1818@gmail.com>
2024-09-18 03:03:48 +0530

    backend:read-from-replica-redis

--- lib/mobility-core/src/Kernel/Beam/Connection/Redis.hs
+++ lib/mobility-core/src/Kernel/Beam/Connection/Redis.hs
@@ -10,20 +10,23 @@ import qualified Kernel.Storage.Hedis.Config as KSHC
 prepareRedisConnectionsDriver :: L.MonadFlow m => ECT.ConnectionConfigDriver -> m ()
 prepareRedisConnectionsDriver ECT.ConnectionConfigDriver {..} = do
   preparePosgreSqlConnection <- L.runIO EnvVars.getPreparePosgreSqlConnection
-  when preparePosgreSqlConnection (prepareRedisClusterConnection hedisClusterCfg)
+  when preparePosgreSqlConnection (prepareRedisClusterConnection hedisClusterCfg >> prepareRedisReplicaConnection hedisClusterCfg)
 
 prepareRedisConnectionsRider :: L.MonadFlow m => ECT.ConnectionConfigRider -> m ()
 prepareRedisConnectionsRider ECT.ConnectionConfigRider {..} = do
   preparePosgreSqlConnection <- L.runIO EnvVars.getPreparePosgreSqlConnection
-  when preparePosgreSqlConnection (prepareRedisClusterConnection hedisClusterCfg)
+  when preparePosgreSqlConnection (prepareRedisClusterConnection hedisClusterCfg >> prepareRedisReplicaConnection hedisClusterCfg)
 
 prepareRedisConnectionsDashboard :: L.MonadFlow m => ECT.ConnectionConfigDashboard -> m ()
 prepareRedisConnectionsDashboard ECT.ConnectionConfigDashboard {..} = do
   preparePosgreSqlConnection <- L.runIO EnvVars.getPreparePosgreSqlConnection
-  when preparePosgreSqlConnection (prepareRedisClusterConnection hedisClusterCfg)
+  when preparePosgreSqlConnection (prepareRedisClusterConnection hedisClusterCfg >> prepareRedisReplicaConnection hedisClusterCfg)
 
 kvRedis :: Text
 kvRedis = "KVRedis"
+
+kvReplicaRedis :: Text
+kvReplicaRedis = "KVReplicaRedis"
 
 prepareRedisClusterConnection :: (HasCallStack, L.MonadFlow m) => KSHC.HedisCfg -> m ()
 prepareRedisClusterConnection KSHC.HedisCfg {..} = do
@@ -39,6 +42,24 @@ prepareRedisClusterConnection KSHC.HedisCfg {..} = do
           connectDatabase = connectDatabase,
           connectMaxConnections = connectMaxConnections,
           connectReadOnly = False,
+          connectMaxIdleTime = connectMaxIdleTime,
+          connectTimeout = connectTimeout
+        }
+
+prepareRedisReplicaConnection :: (HasCallStack, L.MonadFlow m) => KSHC.HedisCfg -> m ()
+prepareRedisReplicaConnection KSHC.HedisCfg {..} = do
+  L.logDebug @Text "prepareDBConnections" ("kvdbConfig:" <> show hedisClusterConf)
+  kvRedisCon <- L.initKVDBConnection (ET.mkKVDBClusterConfig kvReplicaRedis hedisClusterConf)
+  L.throwOnFailedWithLog kvRedisCon L.KVDBConnectionFailedException "Failed to connect to Redis Cluster DB."
+  where
+    hedisClusterConf =
+      ET.RedisConfig
+        { connectHost = connectHost,
+          connectPort = connectPort,
+          connectAuth = connectAuth,
+          connectDatabase = connectDatabase,
+          connectMaxConnections = connectMaxConnections,
+          connectReadOnly = True,
           connectMaxIdleTime = connectMaxIdleTime,
           connectTimeout = connectTimeout
         }
