eacdc371095e50483a76db8e2782f3e234e516cc
rupakkorde <rupak.korde@juspay.in>
2024-11-15 13:49:36 +0530

    backend/enh/driver-stop-detected-Notification-type

--- lib/mobility-core/src/Kernel/External/Notification/Interface/FCM.hs
+++ lib/mobility-core/src/Kernel/External/Notification/Interface/FCM.hs
@@ -115,6 +115,7 @@ interfaceCategoryToFCMNotificationType = \case
   Interface.DRIVER_HAS_REACHED_DESTINATION -> FCM.DRIVER_HAS_REACHED_DESTINATION
   Interface.USER_FAVOURITE_DRIVER -> FCM.USER_FAVOURITE_DRIVER
   Interface.POST_RIDE_SAFETY_CHECK -> FCM.POST_RIDE_SAFETY_CHECK
+  Interface.DRIVER_STOP_DETECTED -> FCM.DRIVER_STOP_DETECTED
 
 interfaceShowNotificationToFCMShowNotification :: Interface.ShowNotification -> FCM.FCMShowNotification
 interfaceShowNotificationToFCMShowNotification = \case
