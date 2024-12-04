0d44f4cfb6cfd514a61cc0be4fd9dfc77c6f2fc6
DwivediAman-exe <19dwivediaman@gmail.com>
2024-12-04 15:33:56 +0530

    Backend/enh/metro-warrior-coin-notification-type

--- lib/mobility-core/src/Kernel/External/Notification/Interface/FCM.hs
+++ lib/mobility-core/src/Kernel/External/Notification/Interface/FCM.hs
@@ -116,6 +116,8 @@ interfaceCategoryToFCMNotificationType = \case
   Interface.USER_FAVOURITE_DRIVER -> FCM.USER_FAVOURITE_DRIVER
   Interface.POST_RIDE_SAFETY_CHECK -> FCM.POST_RIDE_SAFETY_CHECK
   Interface.DRIVER_STOP_DETECTED -> FCM.DRIVER_STOP_DETECTED
+  Interface.TO_METRO_COINS -> FCM.TO_METRO_COINS
+  Interface.FROM_METRO_COINS -> FCM.FROM_METRO_COINS
 
 interfaceShowNotificationToFCMShowNotification :: Interface.ShowNotification -> FCM.FCMShowNotification
 interfaceShowNotificationToFCMShowNotification = \case
--- lib/mobility-core/src/Kernel/External/Notification/Interface/Types.hs
+++ lib/mobility-core/src/Kernel/External/Notification/Interface/Types.hs
@@ -85,6 +85,8 @@ data Category
   | USER_FAVOURITE_DRIVER
   | POST_RIDE_SAFETY_CHECK
   | DRIVER_STOP_DETECTED
+  | TO_METRO_COINS
+  | FROM_METRO_COINS
   deriving (Show, Eq, Read, Generic, Ord, ToSchema, ToJSON, FromJSON)
 
 $(mkBeamInstancesForEnum ''Category)
