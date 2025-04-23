d1de4219268ce99af82038755a06088fc8a33421
Pravinkumar S <spravinkumar9952@gmail.com>
2025-04-23 16:26:21 +0530

    feat: live EKD error types

--- lib/mobility-core/src/Kernel/External/Notification/FCM/Types.hs
+++ lib/mobility-core/src/Kernel/External/Notification/FCM/Types.hs
@@ -194,6 +194,7 @@ data FCMNotificationType
   | FLEET_UNLINK_FROM_OPERATOR
   | DRIVER_REQUEST_REJECTED
   | SAFETY_ALERT_RIDE_STOPPAGE
+  | EKD_LIVE_CALL_FEEDBACK
   deriving (Show, Eq, Read, Ord, Generic, ToJSON, FromJSON)
   deriving (PrettyShow) via Showable FCMNotificationType
 
