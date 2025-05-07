befd4dba7b4c7c084c804ffc847ae2c3dd441e27
aayushagarwal2001 <aayush2001fzd@gmail.com>
2025-05-07 14:01:39 +0530

    backend/enhc : add split in update order

--- lib/mobility-core/src/Kernel/External/Payment/Interface/Types.hs
+++ lib/mobility-core/src/Kernel/External/Payment/Interface/Types.hs
@@ -78,28 +78,29 @@ data CreateOrderReq = CreateOrderReq
 data Split = Split
   { amount :: HighPrecMoney,
     merchantCommission :: HighPrecMoney,
-    subMid :: Text
+    subMid :: Text,
+    uniqueSplitId :: Maybe Text
   }
-  deriving stock (Show, Eq, Generic)
+  deriving (Show, Eq, Generic, FromJSON, ToJSON, ToSchema)
 
 newtype Vendor = Vendor
   { split :: [Split]
   }
-  deriving stock (Show, Eq, Generic)
+  deriving (Show, Eq, Generic, FromJSON, ToJSON, ToSchema)
 
 data SplitSettlementDetails = SplitSettlementDetails
   { marketplace :: Marketplace,
     mdrBorneBy :: MBY,
     vendor :: Vendor
   }
-  deriving stock (Show, Eq, Generic)
+  deriving (Show, Eq, Generic, FromJSON, ToJSON, ToSchema)
 
-data MBY = MARKETPLACE | VENDOR | ALL deriving stock (Show, Eq, Generic)
+data MBY = MARKETPLACE | VENDOR | ALL deriving (Show, Eq, Generic, FromJSON, ToJSON, ToSchema)
 
 newtype Marketplace = Marketplace
   { amount :: HighPrecMoney
   }
-  deriving stock (Show, Eq, Generic)
+  deriving (Show, Eq, Generic, FromJSON, ToJSON, ToSchema)
 
 newtype OrderStatusReq = OrderStatusReq
   { orderShortId :: Text
@@ -536,7 +537,8 @@ data CreateCustomerResp = CreateCustomerResp
 
 data OrderUpdateReq = OrderUpdateReq
   { amount :: HighPrecMoney,
-    orderShortId :: Text
+    orderShortId :: Text,
+    splitSettlementDetails :: Maybe SplitSettlementDetails
   }
   deriving stock (Show, Eq, Generic)
   deriving anyclass (FromJSON, ToJSON, ToSchema)
