fe77a6fe4c835f2406a788a319fb5886542c1f26
Piyush <54761970+piyushKumar-1@users.noreply.github.com>
2025-06-06 14:32:10 +0530

    Revert "backend/enhc : add split in update order"
    This reverts commit befd4dba7b4c7c084c804ffc847ae2c3dd441e27.

--- lib/mobility-core/src/Kernel/External/Payment/Interface/Juspay.hs
+++ lib/mobility-core/src/Kernel/External/Payment/Interface/Juspay.hs
@@ -94,8 +94,7 @@ updateOrder config mRoutingId req = do
       do
         return
           Juspay.OrderUpdateReq
-            { amount = amount,
-              split_settlement_details = mkSplitSettlementDetails <$> splitSettlementDetails
+            { amount = amount
             }
     mkUpdateOrderRes Juspay.OrderUpdateResp {..} =
       OrderUpdateResp
@@ -310,7 +309,7 @@ mkSplitSettlementDetails splitDetails =
   where
     mkMarketplace Marketplace {..} = Juspay.Marketplace {..}
     mkVendor vendor = Juspay.Vendor {split = mkSplit <$> vendor.split}
-    mkSplit split = Juspay.Split {amount = split.amount, merchant_commission = split.merchantCommission, sub_mid = split.subMid, unique_split_id = split.uniqueSplitId}
+    mkSplit split = Juspay.Split {amount = split.amount, merchant_commission = split.merchantCommission, sub_mid = split.subMid}
 
 orderStatus ::
   ( HasCallStack,
