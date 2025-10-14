7ff10ace0e9552840081076c41c694c421855427
KavyashreeS <kavyashree.s1998@gmail.com>
2025-10-14 17:05:00 +0530

    Backend: feat - Fetching basket details from create order response

--- lib/mobility-core/src/Kernel/External/Payment/Juspay/Types/CreateOrder.hs
+++ lib/mobility-core/src/Kernel/External/Payment/Juspay/Types/CreateOrder.hs
@@ -271,7 +271,8 @@ data SDKPayloadDetails = SDKPayloadDetails
     createMandate :: Maybe MandateType,
     mandateMaxAmount :: Maybe Text,
     mandateStartDate :: Maybe Text,
-    mandateEndDate :: Maybe Text
+    mandateEndDate :: Maybe Text,
+    basket :: Maybe Text
   }
   deriving stock (Show, Generic)
   deriving anyclass (ToSchema)
