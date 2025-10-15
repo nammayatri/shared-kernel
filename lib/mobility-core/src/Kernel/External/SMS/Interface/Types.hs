aabb9a26136a9cae10b5b35525fa10715cd6a44c
arnab.kumar <arnab.kumar@nammayatri.in>
2025-10-15 15:48:32 +0530

    tmp

--- lib/mobility-core/src/Kernel/External/SMS/Interface/Types.hs
+++ lib/mobility-core/src/Kernel/External/SMS/Interface/Types.hs
@@ -27,6 +27,7 @@ import Deriving.Aeson
 import qualified Kernel.External.SMS.DigoEngage.Config as DigoEngageSms
 import qualified Kernel.External.SMS.ExotelSms.Config as ExotelSms
 import qualified Kernel.External.SMS.GupShup.Config as GupShup
+import qualified Kernel.External.SMS.Karix.Config as Karix
 import qualified Kernel.External.SMS.MyValueFirst.Config as MyValueFirst
 import qualified Kernel.External.SMS.TwillioSms.Config as TwillioSms
 import qualified Kernel.External.SMS.Types as T
@@ -40,7 +41,7 @@ data SmsHandler m = SmsHandler
     getProviderConfig :: T.SmsService -> m SmsServiceConfig
   }
 
-data SmsServiceConfig = MyValueFirstConfig MyValueFirst.MyValueFirstCfg | ExotelSmsConfig ExotelSms.ExotelSmsCfg | GupShupConfig GupShup.GupShupCfg | TwillioSmsConfig TwillioSms.TwillioSmsCfg | DigoEngageSmsConfig DigoEngageSms.DigoEngageSmsCfg | VonageSmsConfig VonageSms.VonageSmsCfg
+data SmsServiceConfig = MyValueFirstConfig MyValueFirst.MyValueFirstCfg | ExotelSmsConfig ExotelSms.ExotelSmsCfg | GupShupConfig GupShup.GupShupCfg | TwillioSmsConfig TwillioSms.TwillioSmsCfg | DigoEngageSmsConfig DigoEngageSms.DigoEngageSmsCfg | VonageSmsConfig VonageSms.VonageSmsCfg | KarixConfig Karix.KarixCfg
   deriving stock (Show, Eq, Generic)
   deriving (FromJSON, ToJSON) via CustomJSON '[SumTaggedObject "tag" "content"] SmsServiceConfig
 
