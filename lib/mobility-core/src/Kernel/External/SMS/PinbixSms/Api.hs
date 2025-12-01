561d3949c73e61130d74d5389d602e4fe8ccf7df
arnab.kumar <arnab.kumar@nammayatri.in>
2025-12-01 13:31:52 +0530

    added encryption

--- lib/mobility-core/src/Kernel/External/SMS/PinbixSms/Api.hs
+++ lib/mobility-core/src/Kernel/External/SMS/PinbixSms/Api.hs
@@ -4,17 +4,18 @@ module Kernel.External.SMS.PinbixSms.Api where
 
 import EulerHS.Prelude
 import Kernel.External.SMS.PinbixSms.Types
+import Kernel.Types.App
 import Servant
 
 type ServiceAPI =
-  QueryParam' '[Required] "userid" Text
-    :> QueryParam' '[Required] "password" Text
-    :> QueryParam' '[Required] "sendMethod" Text
-    :> QueryParam' '[Required] "senderid" Text
-    :> QueryParam' '[Required] "msgType" Text
-    :> QueryParam' '[Required] "output" Text
-    :> QueryParam' '[Required] "mobile" Text
-    :> QueryParam' '[Required] "msg" Text
+  MandatoryQueryParam "userid" Text
+    :> MandatoryQueryParam "password" Text
+    :> MandatoryQueryParam "sendMethod" Text
+    :> MandatoryQueryParam "senderid" Text
+    :> MandatoryQueryParam "msgType" Text
+    :> MandatoryQueryParam "output" Text
+    :> MandatoryQueryParam "mobile" Text
+    :> MandatoryQueryParam "msg" Text
     :> Get '[JSON] PinbixSmsResponse
 
 pinbixSmsSendAPI :: Proxy ServiceAPI
