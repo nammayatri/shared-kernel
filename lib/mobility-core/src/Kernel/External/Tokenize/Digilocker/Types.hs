6c544098fb42d970659f8e82218949c8c0693b85
Himanshu Mishra <himanshukm2301@gmail.com>
2026-01-09 11:25:54 +0530

    added custom from-json for digilocker response

--- lib/mobility-core/src/Kernel/External/Tokenize/Digilocker/Types.hs
+++ lib/mobility-core/src/Kernel/External/Tokenize/Digilocker/Types.hs
@@ -15,6 +15,9 @@
 
 module Kernel.External.Tokenize.Digilocker.Types where
 
+import Data.Aeson (Value (..), withObject, (.:), (.:?))
+import Data.Aeson.Types (Parser)
+import qualified Data.Text as T
 import Kernel.External.Encryption
 import Kernel.Prelude hiding (error)
 import Web.FormUrlEncoded (ToForm)
@@ -56,4 +59,30 @@ data DigilockerTokenizeResponse = DigilockerTokenizeResponse
     new_account :: Maybe Text
   }
   deriving stock (Show, Eq, Generic)
-  deriving anyclass (FromJSON, ToJSON)
+  deriving anyclass (ToJSON)
+
+instance FromJSON DigilockerTokenizeResponse where
+  parseJSON = withObject "DigilockerTokenizeResponse" $ \obj -> do
+    access_token <- obj .: "access_token"
+    token_type <- obj .:? "token_type"
+    expires_in <- obj .:? "expires_in"
+    scope <- obj .:? "scope"
+    consent_valid_till <- parseConsentValidTill =<< obj .:? "consent_valid_till"
+    digilockerid <- obj .:? "digilockerid"
+    name <- obj .:? "name"
+    dob <- obj .:? "dob"
+    gender <- obj .:? "gender"
+    eaadhaar <- obj .:? "eaadhaar"
+    reference_key <- obj .:? "reference_key"
+    mobile <- obj .:? "mobile"
+    refresh_token <- obj .:? "refresh_token"
+    new_account <- obj .:? "new_account"
+    pure DigilockerTokenizeResponse {..}
+    where
+      parseConsentValidTill :: Maybe Value -> Parser (Maybe Int)
+      parseConsentValidTill Nothing = pure Nothing
+      parseConsentValidTill (Just (Number n)) = pure $ Just (floor n)
+      parseConsentValidTill (Just (String s)) = case readMaybe (T.unpack s) of
+        Just i -> pure $ Just i
+        Nothing -> fail $ "Could not parse consent_valid_till as Int: " <> T.unpack s
+      parseConsentValidTill (Just v) = fail $ "Expected Int or String for consent_valid_till, got: " <> show v
