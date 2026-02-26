c363a5bf0a5d0b38958ab7481f4e2e184c0fe1b9
Shivendra Kumar Shah <92780755+shivendrashah@users.noreply.github.com>
2026-02-26 16:25:11 +0530

    feat: ERSS and GJ112 integration (#1149)
    Co-authored-by: Tejaswini <tejaswini3477@gmail.com>
    Co-authored-by: Ayush Pandey <ayushpandey1903@gmail.com>
    Co-authored-by: Arnab Dutta <arnabduttasocial@gmail.com>

---
+++ lib/mobility-core/src/Kernel/External/SOS/ERSS/Flow.hs
@@ -0,0 +1,146 @@
+{-
+  Copyright 2022-23, Juspay India Pvt Ltd
+
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is
+
+  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+
+  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero
+
+  General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Kernel.External.SOS.ERSS.Flow
+  ( sendInitialSOS,
+    sendSOSTrace,
+    uploadMedia,
+    validateERSSResponse,
+  )
+where
+
+import qualified EulerHS.Language as L
+import qualified EulerHS.Types as ET
+import Kernel.External.Encryption
+import Kernel.External.SOS.ERSS.API
+import Kernel.External.SOS.ERSS.Auth
+import Kernel.External.SOS.ERSS.Config
+import Kernel.External.SOS.ERSS.Types
+import Kernel.Prelude
+import Kernel.ServantMultipart
+import qualified Kernel.Storage.Hedis as Redis
+import Kernel.Tools.Metrics.CoreMetrics (CoreMetrics)
+import Kernel.Types.Common
+import Kernel.Utils.Common
+
+-- | Validate ERSS API response envelope.
+-- C-DAC APIs may return HTTP 200 with resultCode "OPERATION_FAILURE" in the body.
+-- This function checks the resultCode and throws an appropriate ERSSError
+-- if the operation was not successful, ensuring consistent error handling
+-- regardless of whether the failure comes as an HTTP error or API-level error.
+validateERSSResponse ::
+  (MonadFlow m) =>
+  Text ->
+  ERSSApiResponse a ->
+  m (ERSSApiResponse a)
+validateERSSResponse _apiName res
+  | isERSSSuccess res = pure res
+  | otherwise = do
+    let errMsg = fromMaybe "Unknown error" res.errorMsg
+    throwM $ ERSSOperationFailure errMsg
+
+-- | Send Initial SOS Signal to C-DAC ERSS
+sendInitialSOS ::
+  ( EncFlow m r,
+    CoreMetrics m,
+    Redis.HedisFlow m r,
+    MonadFlow m,
+    HasRequestId r,
+    MonadReader r m
+  ) =>
+  ERSSCfg ->
+  ERSSInitialSOSReq ->
+  m ERSSInitialSOSRes
+sendInitialSOS config req = do
+  token <- getERSSToken config
+  let authToken = ERSSAuthToken token.accessToken
+  res <-
+    callERSSAPI
+      config.baseUrl
+      (ET.client erssInitialSOSAPI (Just authToken) req)
+      "erssInitialSOS"
+      erssInitialSOSAPI
+  validateERSSResponse "Initial SOS" res
+
+-- | Send SOS Trace (location update) to C-DAC ERSS
+sendSOSTrace ::
+  ( EncFlow m r,
+    CoreMetrics m,
+    Redis.HedisFlow m r,
+    MonadFlow m,
+    HasRequestId r,
+    MonadReader r m
+  ) =>
+  ERSSCfg ->
+  ERSSTraceReq ->
+  m ERSSTraceRes
+sendSOSTrace config req = do
+  token <- getERSSToken config
+  let authToken = ERSSAuthToken token.accessToken
+  res <-
+    callERSSAPI
+      config.baseUrl
+      (ET.client erssTraceAPI (Just authToken) req)
+      "erssSOSTrace"
+      erssTraceAPI
+  validateERSSResponse "SOS Trace" res
+
+-- | Upload Media File to ERSS
+uploadMedia ::
+  ( EncFlow m r,
+    CoreMetrics m,
+    Redis.HedisFlow m r,
+    MonadFlow m,
+    HasRequestId r,
+    MonadReader r m
+  ) =>
+  ERSSCfg ->
+  Text ->
+  Text ->
+  FilePath ->
+  m ERSSMediaUploadRes
+uploadMedia config phoneNumber fileName filePath = do
+  token <- getERSSToken config
+  let authToken = ERSSAuthToken token.accessToken
+  boundary <- L.runIO genBoundary
+  let multipartData =
+        MultipartData
+          []
+          [ FileData "file" fileName "" filePath
+          ]
+  let apiCall =
+        ET.client
+          erssMediaUploadAPI
+          config.authCode
+          phoneNumber
+          fileName
+          (Just authToken)
+          (boundary, multipartData)
+  res <-
+    callERSSAPI
+      config.baseUrl
+      apiCall
+      "erssMediaUpload"
+      erssMediaUploadAPI
+  validateERSSResponse "Media Upload" res
+
+callERSSAPI :: CallAPI m r api a
+callERSSAPI =
+  callApiUnwrappingApiError
+    (identity @ERSSError)
+    Nothing
+    (Just "ERSS_API_ERROR")
+    Nothing
---
+++ lib/mobility-core/src/Kernel/External/SOS/ERSS/Types.hs
@@ -0,0 +1,249 @@
+{-
+  Copyright 2022-23, Juspay India Pvt Ltd
+
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License
+
+  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is
+
+  distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+
+  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero
+
+  General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Kernel.External.SOS.ERSS.Types where
+
+import Data.Aeson
+import Kernel.Prelude
+import Kernel.Types.Error.BaseError
+import Kernel.Types.Error.BaseError.HTTPError
+import Kernel.Types.Error.BaseError.HTTPError.FromResponse (FromResponse (fromResponse))
+import Servant.Client (ResponseF (responseBody))
+import Web.FormUrlEncoded (ToForm, toForm)
+import Web.Internal.HttpApiData (ToHttpApiData (..))
+
+-- | OAuth Token Request for password grant (initial authentication)
+data ERSSPasswordGrantReq = ERSSPasswordGrantReq
+  { grant_type :: Text,
+    client_id :: Text,
+    client_secret :: Text,
+    username :: Text,
+    password :: Text
+  }
+  deriving (Show, Eq, Generic, ToJSON)
+
+instance ToForm ERSSPasswordGrantReq where
+  toForm ERSSPasswordGrantReq {..} =
+    [ ("grant_type", toQueryParam grant_type),
+      ("client_id", toQueryParam client_id),
+      ("client_secret", toQueryParam client_secret),
+      ("username", toQueryParam username),
+      ("password", toQueryParam password)
+    ]
+
+-- | OAuth Token Request for refresh grant
+data ERSSRefreshGrantReq = ERSSRefreshGrantReq
+  { grant_type :: Text,
+    client_id :: Text,
+    client_secret :: Text,
+    refresh_token :: Text
+  }
+  deriving (Show, Eq, Generic, ToJSON)
+
+instance ToForm ERSSRefreshGrantReq where
+  toForm ERSSRefreshGrantReq {..} =
+    [ ("grant_type", toQueryParam grant_type),
+      ("client_id", toQueryParam client_id),
+      ("client_secret", toQueryParam client_secret),
+      ("refresh_token", toQueryParam refresh_token)
+    ]
+
+-- | OAuth Token Response from Keycloak
+data ERSSAuthResponse = ERSSAuthResponse
+  { access_token :: Text,
+    refresh_token :: Text,
+    expires_in :: Int,
+    refresh_expires_in :: Int,
+    token_type :: Text
+  }
+  deriving (Show, Eq, Generic, FromJSON, ToJSON)
+
+-- | Initial SOS Request matching C-DAC API spec
+data ERSSInitialSOSReq = ERSSInitialSOSReq
+  { id :: Maybe Text,
+    dateTime :: Text,
+    latitude :: Double,
+    longitude :: Double,
+    speed :: Maybe Double,
+    mobileNo :: Text,
+    imeiNo :: Maybe Text,
+    gpsProvider :: Maybe Text,
+    senderName :: Maybe Text,
+    address :: Maybe Text,
+    gpsAccuracy :: Maybe Double,
+    stateCode :: Maybe Text,
+    silentCommunication :: Maybe Text,
+    specialNeeds :: Maybe Text,
+    dob :: Maybe Text,
+    gender :: Maybe Text,
+    attachmentFileName :: Maybe Text,
+    authId :: Text,
+    authCode :: Text
+  }
+  deriving (Show, Eq, Generic, ToJSON, FromJSON, ToSchema)
+
+-- | Generic ERSS API Response envelope (resultCode, resultString, errorMsg, message, payLoad)
+data ERSSApiResponse a = ERSSApiResponse
+  { resultCode :: Text,
+    resultString :: Maybe Text,
+    errorMsg :: Maybe Text,
+    message :: Maybe Text,
+    payLoad :: Maybe a
+  }
+  deriving (Show, Eq, Generic)
+
+instance (FromJSON a) => FromJSON (ERSSApiResponse a)
+
+instance (ToJSON a) => ToJSON (ERSSApiResponse a)
+
+instance (ToSchema a) => ToSchema (ERSSApiResponse a)
+
+-- | Payload for Initial SOS success response
+data ERSSSOSPayload = ERSSSOSPayload
+  { signalId :: Int,
+    status :: Text,
+    remarks :: Maybe Text
+  }
+  deriving (Show, Eq, Generic, FromJSON, ToJSON, ToSchema)
+
+-- | Payload for SOS Trace success response
+data ERSSTracePayload = ERSSTracePayload
+  { signalId :: Int,
+    status :: Text,
+    statusUpdateTime :: Maybe Text,
+    remarks :: Maybe Text
+  }
+  deriving (Show, Eq, Generic, FromJSON, ToJSON, ToSchema)
+
+-- | Type alias: Initial SOS Response from C-DAC
+type ERSSInitialSOSRes = ERSSApiResponse ERSSSOSPayload
+
+-- | SOS Trace Request for location updates (PACKET field)
+newtype ERSSTraceReq = ERSSTraceReq
+  { packet :: Text
+  }
+  deriving (Show, Eq, Generic)
+
+instance ToJSON ERSSTraceReq where
+  toJSON (ERSSTraceReq pkt) = object ["PACKET" .= pkt]
+
+instance FromJSON ERSSTraceReq where
+  parseJSON = withObject "ERSSTraceReq" $ \o ->
+    ERSSTraceReq <$> o .: "PACKET"
+
+instance ToSchema ERSSTraceReq
+
+-- | Type alias: SOS Trace Response from C-DAC
+type ERSSTraceRes = ERSSApiResponse ERSSTracePayload
+
+-- | Inbound Status Update from C-DAC (Section 7)
+data ERSSStatusUpdateReq = ERSSStatusUpdateReq
+  { idSource :: Maybe Text,
+    idErss :: Maybe Text,
+    currentStatus :: Text,
+    statusDesc :: Maybe Text,
+    comments :: Maybe Text,
+    lastUpdatedTime :: Maybe Int
+  }
+  deriving (Show, Eq, Generic, FromJSON, ToJSON, ToSchema)
+
+-- | Type alias: Status Update Response (acknowledgment we send back to C-DAC)
+type ERSSStatusUpdateRes = ERSSApiResponse Value
+
+-- | Type alias: Media Upload Response from C-DAC
+type ERSSMediaUploadRes = ERSSApiResponse Value
+
+-- | ERSS API Error types matching C-DAC API failure response format.
+-- C-DAC failure responses follow this structure:
+-- { "resultCode": "OPERATION_FAILURE", "resultString": null,
+--   "errorMsg": "<description>", "payLoad": null }
+data ERSSError
+  = ERSSOperationFailure Text
+  | ERSSAuthError Text
+  | ERSSUnknownError Text
+  deriving (Eq, Show, IsBecknAPIError)
+
+instanceExceptionWithParent 'HTTPException ''ERSSError
+
+instance IsBaseError ERSSError where
+  toMessage = \case
+    ERSSOperationFailure msg -> Just $ "ERSS Operation Failed: " <> msg
+    ERSSAuthError msg -> Just $ "ERSS Auth Error: " <> msg
+    ERSSUnknownError msg -> Just $ "ERSS Error: " <> msg
+
+instance IsHTTPError ERSSError where
+  toErrorCode = \case
+    ERSSOperationFailure _ -> "ERSS_OPERATION_FAILURE"
+    ERSSAuthError _ -> "ERSS_AUTH_ERROR"
+    ERSSUnknownError _ -> "ERSS_UNKNOWN_ERROR"
+
+  toHttpCode = \case
+    ERSSOperationFailure _ -> E500
+    ERSSAuthError _ -> E401
+    ERSSUnknownError _ -> E500
+
+instance IsAPIError ERSSError
+
+instance FromResponse ERSSError where
+  fromResponse resp = do
+    let body = responseBody resp
+    case decode body of
+      Just err -> Just err
+      Nothing -> Just $ ERSSUnknownError "Failed to parse ERSS error response"
+
+instance FromJSON ERSSError where
+  parseJSON = withObject "ERSSError" $ \o -> do
+    mResultCode <- o .:? "resultCode"
+    mErrorMsg <- o .:? "errorMsg"
+    mKeycloakErr <- o .:? "error"
+    mKeycloakDesc <- o .:? "error_description"
+    case (mResultCode :: Maybe Text) of
+      Just "OPERATION_FAILURE" ->
+        pure $ ERSSOperationFailure (fromMaybe "Unknown error" mErrorMsg)
+      _ -> case (mKeycloakErr :: Maybe Text) of
+        Just err ->
+          pure $ ERSSAuthError (err <> maybe "" (": " <>) mKeycloakDesc)
+        Nothing ->
+          pure $ ERSSUnknownError (fromMaybe (fromMaybe "Unknown error" mKeycloakDesc) mErrorMsg)
+
+instance ToJSON ERSSError where
+  toJSON err =
+    object
+      [ "resultCode" .= ("OPERATION_FAILURE" :: Text),
+        "resultString" .= Null,
+        "errorMsg" .= toErrMsg err,
+        "payLoad" .= Null
+      ]
+    where
+      toErrMsg = \case
+        ERSSOperationFailure msg -> msg
+        ERSSAuthError msg -> msg
+        ERSSUnknownError msg -> msg
+
+isERSSSuccess :: ERSSApiResponse a -> Bool
+isERSSSuccess res = res.resultCode == "OPERATION_SUCCESS"
+
+-- | Bearer token for Authorization header
+newtype ERSSAuthToken = ERSSAuthToken Text
+  deriving (Show, Eq, Generic)
+
+instance ToHttpApiData ERSSAuthToken where
+  toUrlPiece (ERSSAuthToken token) = "Bearer " <> token
