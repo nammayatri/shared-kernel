1421a8d524225ac2bb6ce9788aa6dd0d234300a2
Hari Krishna <harikrishnadupaguntla1@gmail.com>
2026-05-11 15:38:46 +0530

    added txn_list in the orderStatusResp

--- lib/mobility-core/src/Kernel/External/Payment/Juspay/Types/CreateOrder.hs
+++ lib/mobility-core/src/Kernel/External/Payment/Juspay/Types/CreateOrder.hs
@@ -86,7 +86,7 @@ data PaymentFlows = PaymentFlows
 
 data PaymentFlowStatus = PaymentFlowStatus
   { status :: Text,
-    info :: Maybe PaymentFlowInfo
+    info :: PaymentFlowInfo
   }
   deriving stock (Show, Eq, Generic)
   deriving anyclass (FromJSON, ToJSON, ToSchema)
