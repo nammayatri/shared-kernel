8f36f5d7b3ec70d434f60f3858ff0953af4aa7a9
anu19981 <anusha.d@juspay.in>
2026-06-29 18:06:04 +0530

    sk/aarokya-onboard

--- lib/mobility-core/src/Kernel/External/PartnerSdk/Aarokya/Types.hs
+++ lib/mobility-core/src/Kernel/External/PartnerSdk/Aarokya/Types.hs
@@ -12,7 +12,12 @@ data AarokyaIdProof = AarokyaIdProof
 data AarokyaTokenRequest = AarokyaTokenRequest
   { phone_country_code :: Text,
     phone_number :: Text,
-    id_proof :: AarokyaIdProof
+    id_proof :: AarokyaIdProof,
+    dob :: Maybe Text,
+    address :: Maybe Text,
+    gender :: Maybe Text,
+    first_name :: Maybe Text,
+    last_name :: Maybe Text
   }
   deriving (Show, Eq, Generic, ToJSON, FromJSON)
 
